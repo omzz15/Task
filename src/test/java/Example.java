aad4084976710f6b12cf84443b83e6484c77bf43
dromp <drompatel5@gmail.com>
2022-11-03 18:29:14 -0400

    v3 of the event manager

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,11 +1,22 @@
 import om.self.task.core.EventManager;
-import om.self.task.core.Group;
-import om.self.task.core.Task;
-import om.self.task.other.IncrementedTask;
-import org.w3c.dom.ls.LSOutput;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Example {
     public static void main(String[] args) {
+        EventManager em1 = new EventManager("main");
+        em1.attachToEvent(EventManager.CommonTrigger.START, "main start", () -> System.out.println("main started"));
+
+        EventManager em2 = new EventManager("robot", em1);
+        em2.attachToEvent(EventManager.CommonTrigger.START, "robot start", () -> System.out.println("robot started"));
+
+        EventManager em3 = new EventManager("drive", em2);
+        em3.attachToEvent(EventManager.CommonTrigger.START, "drive start", () -> System.out.println("drive started"));
+
+        System.out.println(em3.getDir());
+
+
 //        Group g1 = new Group("g");
 //        g1.setMaxActiveRunnables(1);
 //
@@ -29,12 +40,5 @@ public class Example {
 //            g1.run();
 //
 //        System.out.println(g1);
-        Runnable r = () -> System.out.println("start event triggered!");
-
-        EventManager eventManager = new EventManager();
-
-        eventManager.attachToEvent(EventManager.CommonTrigger.START, r);
-
-        eventManager.triggerEvent(EventManager.CommonTrigger.START);
     }
 }
